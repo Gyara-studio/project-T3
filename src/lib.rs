d86d7298ec1fcd488923435aed606099b85db82b
gyara <me@gyara.moe>
2021-05-19 18:57:22 +0900

    link rust to godot success

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
+#[cfg(feature = "gd")]
+use gdnative::godot_init;
+#[cfg(feature = "gd")]
+use gd::init;
+
+#[cfg(feature = "gd")]
+mod gd {
+    use gdnative::prelude::*;
+    #[derive(NativeClass)]
+    #[inherit(Node)]
+    pub struct Solver;
+
+    #[methods]
+    impl Solver {
+        fn new(_owner: &Node) -> Self {
+            Self
+        }
+
+        #[export]
+        fn solver(&self, _owner: &Node, input: Vec<String>) -> String {
+            "00".to_owned()
+        }
+    }
+
+    pub(super) fn init(handle: InitHandle) {
+        handle.add_class::<Solver>();
+    }
+}
+
+#[cfg(feature = "gd")]
+godot_init!(init);
+
 #[cfg(test)]
 mod tests {
     #[test]
